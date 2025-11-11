57db43fe5f45c6a21369f2f0b639095c80477033
Manish Sonawane <manishsonawane3010@gmail.com>
2025-11-11 09:46:33 +0000

    please just do as i say

---
+++ src/lib/db.js
@@ -0,0 +1,14 @@
+import { MONGODB_URI } from "../../../config/config";
+import mongoose from "mongoose";
+
+const _db = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
+
+export default _db;
